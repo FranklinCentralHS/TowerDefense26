3d8d315f0834471533e9509d037b29e19ee4cab3
itsEmma26 <ecrone@ftstudent.org>
2024-09-19 14:07:38 -0400

    Death screen, home screen
    added a quit button, replay button, killzone

--- Enemy.gd
+++ Enemy.gd
@@ -1,16 +1,18 @@
 extends CharacterBody2D
 class_name Enemy
 
-@export var speed = 50
-@export var damage = 40
-@export var health = 20
-@export var can_fly = "true" 
+@export var hp= 10
+@export var speed=50 
+@export var  dammage=10 
+@export var can_fly= "false"
+@export var Name= "Enemy"
+@export var can_be_hit= "true"
 
 var accel =7
 @onready var nav: NavigationAgent2D = $NavigationAgent2D 
 func _process (delta):
     var direction = Vector3()
-    nav.target_position = Vector2(575, 115 )
+    nav.target_position = Vector2(575, 115)
 
     direction = nav.get_next_path_position()-global_position 
     direction = direction.normalized()
---
+++ killzone.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var hp= 50
+
+func _process(delta):
+	if hp <= 0:
+		 
+
+
+
